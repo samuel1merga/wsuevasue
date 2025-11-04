447bf25a523f48ee2ca5daa244519cd559d6dd74
Samuel Merga <samuel1merga1@gmail.com>
2025-11-04 04:15:53 +0300

    'changed'

--- wsuevasue/settings.py
+++ wsuevasue/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'sami@123')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
+DEBUG = os.getenv('DEBUG', 'False').lower() == 'false'
 
 ALLOWED_HOSTS = ['wsuevasue.onrender.com', '127.0.0.1', 'localhost']
 
