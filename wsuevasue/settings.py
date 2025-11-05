78dc6c50d81db6e3f4826f2deed40ee6177bcd1f
Samuel Merga <samuel1merga1@gmail.com>
2025-11-05 17:20:44 +0300

    corect requirements

--- wsuevasue/settings.py
+++ wsuevasue/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'sami@123')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = ['your-app-name.onrender.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['wsuevasue.onrender.com', '127.0.0.1', 'localhost']
 
 # Application definition
 INSTALLED_APPS = [
